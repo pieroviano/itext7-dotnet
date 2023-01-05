d7c79acdea770d1a14eb7e9530016ce8e56f389e
Piero Viano <piero.viano@gmail.com>
2023-01-05 02:59:44 +0200

    ''

--- Build.bat
+++ Build.bat
@@ -1,10 +1,6 @@
 IF NOT DEFINED Configuration SET Configuration=Release
 MSBuild.exe iTextCore.sln -t:restore -p:RestorePackagesConfig=true
 MSBuild.exe iTextCore.sln -m /property:Configuration=%Configuration%
-cd NuSpecs
-nuget pack -OutputDirectory ..\Packages Net4x.itext7-commons.nuspec
-nuget pack -OutputDirectory ..\Packages Net4x.itext7.nuspec
-cd ..
 git add -A
 git commit -a --allow-empty-message -m ''
 git push
