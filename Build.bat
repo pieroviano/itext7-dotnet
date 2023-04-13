83ec4a064ec5d6e68bcd575607e0c200e5aa1681
Piero Viano <piero.viano@gmail.com>
2023-04-14 02:30:42 +0300

    ''

--- Build.bat
+++ Build.bat
@@ -1,4 +1,5 @@
 IF NOT DEFINED Configuration SET Configuration=Release
+MSBuild.exe iTextCore.sln -t:clean
 MSBuild.exe iTextCore.sln -t:restore -p:RestorePackagesConfig=true
 MSBuild.exe iTextCore.sln -m /property:Configuration=%Configuration%
 git add -A
